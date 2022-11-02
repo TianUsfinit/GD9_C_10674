2d98a4a545cec4072ebce8cb96ec54bc6e207027
TianUsfinit <112057192+TianUsfinit@users.noreply.github.com>
2022-11-02 21:08:33 +0700

    Proximity

--- app/src/main/java/com/example/gd9_c_10674/MainActivity.kt
+++ app/src/main/java/com/example/gd9_c_10674/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gd9_c_10674
-import android.graphics.Color
+
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -7,72 +8,60 @@ import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatDelegate
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity(), SensorEventListener{
-    private lateinit var sensorManager: SensorManager
-    private lateinit var square: TextView
+class MainActivity : AppCompatActivity() {
+    lateinit var sensorStatusTV: TextView
+    lateinit var proximitySensor: Sensor
+    lateinit var sensorManager: SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Keeps phone in light mode
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        // on below line we are initializing our all variables.
+        sensorStatusTV = findViewById(R.id.idTVSensorStatus)
 
-        square = findViewById(R.id.tv_square)
+        // on below line we are initializing our sensor manager
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
-        setUpSensorStuff()
-    }
+        // on below line we are initializing our proximity sensor variable
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
 
-    private fun setUpSensorStuff(){
-        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-
-        //specify the sensor you want to listen to
-        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+        // on below line we are checking if the proximity sensor is null
+        if (proximitySensor == null) {
+            // on below line we are displaying a toast if no sensor is available
+            Toast.makeText(this, "No proximity sensor found in device..", Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            // on below line we are registering
+            // our sensor with sensor manager
             sensorManager.registerListener(
-                this,
-                accelerometer,
-                SensorManager.SENSOR_DELAY_FASTEST,
-                SensorManager.SENSOR_DELAY_FASTEST
+                proximitySensorEventListener,
+                proximitySensor,
+                SensorManager.SENSOR_DELAY_NORMAL
             )
         }
     }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        // Checks for sensor we have registered
-        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
-            // Log.d ("Main", "onSensorChanged: sides ${event.values[0]} front/back ${event.values[1]} ")
+    var proximitySensorEventListener: SensorEventListener? = object : SensorEventListener{
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
-            //Sides = Tilting phone left(10) and right(-10)
-            val sides = event.values[0]
-
-            // Up/Down = Tilting phone up(10), flat (0), upside-down(10)
-
-            val upDown = event.values[1]
-
-            square.apply {
-                rotationX = upDown * 3f
-                rotationY = sides * 3f
-                rotation = -sides
-                translationX = sides * -10
-                translationY = upDown * 10
-            }
-
-            //Changes the colour of the square if it's completely flat
-            val color = if (upDown.toInt() == 0 && sides.toInt() == 0) Color.GREEN else Color.RED
-            square.setBackgroundColor(color)
-
-            square.text = "up/down ${upDown.toInt()}\nleft/right${sides.toInt()}"
         }
-    }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        return
-    }
-
-    override fun onDestroy() {
-        sensorManager.unregisterListener(this)
-        super.onDestroy()
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
+                if (event.values[0] == 0f) {
+                    // here we are setting our status to our textview..
+                    // if sensor event return 0 then object is closed
+                    // to sensor else object is away from sensor.
+                    sensorStatusTV.text = "<<< Near >>>"
+                } else {
+                    // on below line we are setting text for text view
+                    // as object is away from sensor.
+                    sensorStatusTV.text = "<<<< Away >>>>"
+                }
+            }
+        }
     }
 }
